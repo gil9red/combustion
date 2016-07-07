14536c01978d2f1590a308505696a1e4755621ad
ipetrash <gil9red@gmail.com>
2016-07-07 10:27:31 +0500

    Добавлено todo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,33 @@
 #include <QDebug>
 
 
+// TODO: проверка исключений в наследнике QApplication
+/*
+#include <QtGui>
+#include <QApplication>
+class MyApplication : public QApplication {
+public:
+  MyApplication(int& argc, char ** argv) :
+    QApplication(argc, argv) { }
+  virtual ~MyApplication() { }
+
+  // reimplemented from QApplication so we can throw exceptions in slots
+  virtual bool notify(QObject * receiver, QEvent * event) {
+    try {
+      return QApplication::notify(receiver, event);
+    } catch(std::exception& e) {
+      qCritical() << "Exception thrown:" << e.what();
+    }
+    return false;
+  }
+
+int main(int argc, char *argv[]) {
+  MyApplication app(argc, argv);
+  // ...
+}
+*/
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
